ffb566aad3c9478e121f4914d567cfaf74ab160a
Haisam K. Ido <haisam.ido@nasa.gov>
2019-05-18 13:53:42 -0400

    Created Makefile and linked to https://cloud.docker.com/repository/docker/haisamido/telemanom for auto builds

--- docker/docker-do.sh
+++
@@ -1,9 +0,0 @@
-
-docker build -t telemanom .
-
-docker-compose up
-
-docker-compose down
-# -v --rmi all --remove-orphans
-#docker rm -v $(docker ps -aq -f 'status=exited')
-#docker rmi $(docker images -aq -f 'dangling=true')
